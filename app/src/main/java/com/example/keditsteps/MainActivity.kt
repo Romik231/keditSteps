b300a94a1d2e2ea90de167b501272df006a42cfc
Roman Stepurov <rstepurov@livemaster.ru>
2021-03-29 23:01:19 +0300

    забабахал пару кнопок

--- app/src/main/java/com/example/keditsteps/MainActivity.kt
+++ app/src/main/java/com/example/keditsteps/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.keditsteps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView2)
+
     }
+
+    fun toastMe(view: View)
+    {
+        val a = view
+        val myToast = Toast.makeText(this, "touched Me", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun count(view: View) {
+        val count = textView.text.toString()
+        var countParse: Int = Integer.parseInt(count)
+        countParse++
+        textView.text = countParse.toString()
+    }
+
+    fun reset(view: View)
+    {
+        textView.text = "0"
+
+    }
+
 }
