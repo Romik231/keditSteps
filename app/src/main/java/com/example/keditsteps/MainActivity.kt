b0917afaabd63f3aacd55a5a3eba79e7f805c2b9
Roman Stepurov <rstepurov@livemaster.ru>
2021-03-30 22:23:10 +0300

    экран с интентом

--- app/src/main/java/com/example/keditsteps/MainActivity.kt
+++ app/src/main/java/com/example/keditsteps/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.keditsteps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,10 +32,16 @@ class MainActivity : AppCompatActivity() {
         textView.text = countParse.toString()
     }
 
-    fun reset(view: View)
-    {
-        textView.text = "0"
 
+    fun randomIt(view: View)
+    {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        val startVal = textView.text.toString()
+        val toIntVal = Integer.parseInt(startVal)
+
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, toIntVal)
+
+        startActivity(randomIntent)
     }
 
 }
