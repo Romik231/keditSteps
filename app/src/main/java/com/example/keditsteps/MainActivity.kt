a5b53b9e59073c24b5d79cb130e0aaf63945a447
Roman Stepurov <rstepurov@livemaster.ru>
2021-04-06 22:54:51 +0300

    Вернул приложение в исходное состояние

--- app/src/main/java/com/example/keditsteps/MainActivity.kt
+++ app/src/main/java/com/example/keditsteps/MainActivity.kt
@@ -1,66 +1,15 @@
 package com.example.keditsteps
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textView: TextView
-    lateinit var myObserver: MyObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView2)
-        myObserver = MyObserver()
-        lifecycle.addObserver(myObserver)
+
 
     }
-
-    fun toastMe(view: View)
-    {
-        val a = view
-        val myToast = Toast.makeText(this, "touched Me", Toast.LENGTH_SHORT)
-        myToast.show()
-    }
-
-    fun count(view: View) {
-        val count = textView.text.toString()
-        var countParse: Int = Integer.parseInt(count)
-        countParse++
-        textView.text = countParse.toString()
-    }
-
-
-    fun randomIt(view: View)
-    {
-        val randomIntent = Intent(this, SecondActivity::class.java)
-        val startVal = textView.text.toString()
-        val toIntVal = Integer.parseInt(startVal)
-
-        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, toIntVal)
-
-        startActivity(randomIntent)
-    }
-
-    fun addTextThirdActivity(view: View)
-    {
-        val thirdIntent = Intent(this, Third::class.java)
-        val text = "text from first activity"
-        val text2 = "текст для второго view"
-        thirdIntent.putExtra(Third.TEXT_FROM_FIRST_ACTIVITY, text)
-        thirdIntent.putExtra(Third.TEXT_SECOND_VIEW, text2)
-        startActivity(thirdIntent)
-    }
-
-    fun stateActivity(view: View)
-    {
-        val intent = Intent(this, StateActivity::class.java)
-        startActivity(intent)
-    }
-
 }
